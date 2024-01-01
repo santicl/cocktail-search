f515da326febc616e7697a7b067c685c65ac41f9
santicl <caballerosuarezsantiago@gmail.com>
2024-01-01 12:27:05 -0500

    firts

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -9,6 +9,7 @@ function Search() {
     const [cocktailItem, setCocktailItem] = useState({});
     const [loading, setLoading] = useState(true);
     const [openSelect, setOpenSelect] = useState(false);
+    const [showDelete, setShowDelete] = useState(false);
 
     const openOption = () => {
         setOpenSelect(!openSelect);
@@ -17,6 +18,21 @@ function Search() {
     const selectValue = (e) => {
         leagueInput.current.value = e.target.outerText;
         setOpenSelect(!openSelect);
+        if (leagueInput.current.value === '') {
+            setShowDelete(false);
+        } else {
+            setShowDelete(true);
+        }
+    }
+
+    const toggleDelete = () => {
+        if (leagueInput.current.value === '') {
+            console.log(leagueInput.current.value)
+        } else {
+            leagueInput.current.value = '';
+            setShowDelete(!showDelete);
+            console.log(leagueInput.current.value)
+        }
     }
 
     const leagueInput = useRef();
@@ -126,6 +142,12 @@ function Search() {
                                                 placeholder='Selecciona Ingrediente'
                                                 readOnly
                                             />
+
+                                            <button onClick={toggleDelete} className={showDelete === false ? 'Button-Delete' : 'Button-Delete active'}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                                                </svg>
+                                            </button>
                                         </div>
 
                                         <div className={openSelect ? 'options active' : 'options'}>
